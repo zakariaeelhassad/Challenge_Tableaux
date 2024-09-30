c12bdcecc8208a0563081219b0ac01bd973c88a2
zakariaeelhassad <zakariaelhassad@gmail.com>
2024-09-30 14:40:26 +0100

    ajout file

---
+++ challenge1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int T[5] = {1,2,3,4,5};
+
+    printf("entrer les elements du tableau : \n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n",T[i]);
+    }
+
+}
